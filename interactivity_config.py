2a0b491e8be0d4e30e1cadb020a53fbed93a645a
mona <monaxwang@gmail.com>
2023-08-13 18:30:54 -0400

    Button matrix can control languages

--- interactivity_config.py
+++ interactivity_config.py
@@ -21,6 +21,13 @@ keys_by_language: dict[str, str] = {
     "turkish": "t"
 }
 
+button_by_language: dict[str, tuple] = {
+    "brazilian_portuguese": (0, 0),
+    "kurdish": (0, 1),
+    "hakka": (1, 0),
+    "russian": (1, 1),
+}
+
 button_numbers_by_language: dict[str, int] = {
     "abkhazian": "a",
     "brazilian_portuguese": "b",
